9615721418a13d45a0efc3790fe436dab4f90633
Cesar Colunga <ccolungar@gmail.com>
2021-03-03 12:55:19 -0600

    Curso flask - clase Implementación de Flask-Bootstrap y Flask-WTF

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from flask import Flask, request, make_response, redirect, render_template, session
 from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -10,6 +13,12 @@ todos = ['Comprar cafe', 'Enviar solicitud de compra',
          'Entregar video a producto']
 
 # Los decoradores que usen route se les llama vistas
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Nombre de usuario', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
 
 
 @app.errorhandler(404)
@@ -36,9 +45,11 @@ def index():
 @app.route("/hello")
 def hello():
     user_ip = session.get("user_ip")
+    login_form = LoginForm()
     context = {
         'user_ip': user_ip,
-        'todos': todos
+        'todos': todos,
+        'login_form': login_form
     }
 
     return render_template("hello.html", **context)
