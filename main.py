5dbefdc4a1a6faaf1a2f5a72b8fd814130858014
Cesar Colunga <ccolungar@gmail.com>
2021-03-04 20:11:26 -0600

    Curso flask - proyecto autenticacion de usuarios logout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import unittest
 from flask import request, make_response, redirect, render_template, session, url_for, flash
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 from app.firestore_service import get_users
 from app.firestore_service import get_todos
@@ -46,7 +46,7 @@ def index():
 @login_required
 def hello():
     user_ip = session.get("user_ip")
-    username = session.get('username')
+    username = current_user.id
 
     context = {
         'user_ip': user_ip,
