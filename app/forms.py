78f63ad090efe0cdbc3ca0a6e2ba6cb4de182cff
Cesar Colunga <ccolungar@gmail.com>
2021-03-05 09:45:22 -0600

    Curso de flask - proyecto editar tareas

--- app/forms.py
+++ app/forms.py
@@ -12,3 +12,7 @@ class LoginForm(FlaskForm):
 class TodoForm(FlaskForm):
     description = StringField('Descripcion', validators=[DataRequired()])
     submit = SubmitField('Crear')
+
+
+class DeleteTodoForm(FlaskForm):
+    submit = SubmitField('Borrar')
