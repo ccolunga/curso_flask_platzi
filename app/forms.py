611b0f83d9b3a72f5470534a4f48508d20cdc2bb
Cesar Colunga <ccolungar@gmail.com>
2021-03-06 18:28:00 -0600

    Quitar archivos de gitignore

--- app/forms.py
+++ app/forms.py
@@ -5,7 +5,7 @@ from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    password = PasswordField('Contraseña', validators=[DataRequired()])
     submit = SubmitField('Iniciar sesión')
 
 
